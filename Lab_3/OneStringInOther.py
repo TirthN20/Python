304c25beaec366f48a3433a3f9d486586fb3874f
TirthN20 <tirthnandaniya@gmail.com>
2025-04-25 17:48:15 +0530

    Add files via upload

---
+++ Lab_3/OneStringInOther.py
@@ -0,0 +1,10 @@
+def check():
+    a=input("Enter first string : ")
+    b=input("Enter second string : ")
+    if (a in b) :
+        print("First string is present in second string ")
+    elif (b in a) :
+        print("Second string is present in first string ")
+    else :
+        print("No common strings present ")
+check()
