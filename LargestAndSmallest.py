69f869128e9198124156279cfc031a2acd929118
TirthN20 <tirthnandaniya@gmail.com>
2025-04-25 18:24:35 +0530

    Delete LargestAndSmallest.py

--- LargestAndSmallest.py
+++
@@ -1,9 +0,0 @@
-def SL():
-    a=float(input("Enter a number : "))
-    b=float(input("Enter second number : "))
-    if a>b :
-        print(a," is largest and ",b," is smallest")
-    else :
-        print(b," is largest and ",a," is smallest")
-SL()
-SL()
